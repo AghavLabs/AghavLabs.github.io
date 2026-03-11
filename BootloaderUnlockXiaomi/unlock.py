10752b0fd9a03421c195720d4549af2a6fb88fc5
AghavLabs <aaghav@icloud.com>
2026-03-11 10:33:58 +0530

    Update unlock.py

--- BootloaderUnlockXiaomi/unlock.py
+++ BootloaderUnlockXiaomi/unlock.py
@@ -7,7 +7,7 @@ import os
 import importlib
 
 while True:
-    for lib in ['requests', 'ntplib']:
+    for lib in ['requests','ntplib']:
         try:
             importlib.import_module(lib)
         except ModuleNotFoundError:
@@ -16,130 +16,180 @@ while True:
     else:
         break
 
-import requests, json, hashlib, urllib.parse, time, sys, os, base64, ntplib
-from datetime import datetime, timedelta, timezone
-from urllib.parse import parse_qs, urlparse, quote
+import requests,json,hashlib,time,sys,base64,ntplib
+from datetime import datetime,timedelta,timezone
+from urllib.parse import parse_qs,urlparse,quote
 
-version = "1.5.3"
+version="1.5.3"
 
-CYAN = "\033[96m"
-GREEN = "\033[92m"
-YELLOW = "\033[93m"
-WHITE = "\033[97m"
-BOLD = "\033[1m"
-RESET = "\033[0m"
+User="okhttp/4.12.0"
+headers={"User-Agent":User}
 
-print(
-    f"\n{CYAN}{BOLD}╔══════════════════════════════════════╗{RESET}\n"
-    f"{CYAN}{BOLD}║{RESET}    {WHITE}{BOLD}[V{version}] For issues or feedback{RESET}   {CYAN}{BOLD}║{RESET}\n"
-    f"{CYAN}{BOLD}╠══════════════════════════════════════╣{RESET}\n"
-    f"{CYAN}{BOLD}║   {RESET} {YELLOW}Mi Community Bootloader Tool{RESET}       {CYAN}{BOLD}║{RESET}\n"
-    f"{CYAN}{BOLD}╚══════════════════════════════════════╝{RESET}\n"
-)
-
-User = "okhttp/4.12.0"
-headers = {"User-Agent": User}
+print("\nMi Community Bootloader Unlock Tool\n")
 
 def login():
-    base_url = "https://account.xiaomi.com"
-    sid = "18n_bbs_global"
 
-    print(
-        f"\n{CYAN}{BOLD}╔══════════════════════════════════════╗{RESET}"
-        f"\n{CYAN}{BOLD}║{RESET}   {WHITE}{BOLD}     Mi Account LOGIN PANEL{RESET}        {CYAN}{BOLD}║{RESET}"
-        f"\n{CYAN}{BOLD}╚══════════════════════════════════════╝{RESET}"
-    )
+    base_url="https://account.xiaomi.com"
+    sid="18n_bbs_global"
 
-    user = input(f"{YELLOW}➤ Enter Mi ID:{RESET} ")
-    pwd = input(f"{YELLOW}➤ Enter Mi Pwd :{RESET} ")
+    print("\nMi Account Login\n")
 
-    hash_pwd = hashlib.md5(pwd.encode()).hexdigest().upper()
-    cookies = {}
+    user=input("Enter Mi ID: ")
+    pwd=input("Enter Mi Password: ")
+
+    hash_pwd=hashlib.md5(pwd.encode()).hexdigest().upper()
+    cookies={}
 
     def parse(res):
         return json.loads(res.text[11:])
 
-    r = requests.get(f"{base_url}/pass/serviceLogin", params={'sid': sid, '_json': True}, headers=headers, cookies=cookies)
-    cookies.update(r.cookies.get_dict())
-
-    deviceId = cookies["deviceId"]
-
-    data = {k: v[0] for k, v in parse_qs(urlparse(parse(r)['location']).query).items()}
-    data.update({'user': user, 'hash': hash_pwd})
-
-    r = requests.post(f"{base_url}/pass/serviceLoginAuth2", data=data, headers=headers, cookies=cookies)
-    cookies.update(r.cookies.get_dict())
-    res = parse(r)
-
-    if res["code"] == 70016: exit("invalid user or pwd")
-
-    region = json.loads(requests.get(
-        "https://account.xiaomi.com/pass/user/login/region",
+    r=requests.get(
+        f"{base_url}/pass/serviceLogin",
+        params={'sid':sid,'_json':True},
         headers=headers,
         cookies=cookies
-    ).text[11:])["data"]["region"]
+    )
 
-    nonce, ssecurity = res['nonce'], res['ssecurity']
-    res['location'] += f"&clientSign={quote(base64.b64encode(hashlib.sha1(f'nonce={nonce}&{ssecurity}'.encode()).digest()))}"
+    cookies.update(r.cookies.get_dict())
 
-    serviceToken = requests.get(res['location'], headers=headers, cookies=cookies).cookies.get_dict()
+    deviceId=cookies["deviceId"]
 
-    micdata = {
-        "userId": res['userId'],
-        "new_bbs_serviceToken": serviceToken["new_bbs_serviceToken"],
-        "region": region,
-        "deviceId": deviceId
+    data={k:v[0] for k,v in parse_qs(urlparse(parse(r)['location']).query).items()}
+    data.update({'user':user,'hash':hash_pwd})
+
+    r=requests.post(
+        f"{base_url}/pass/serviceLoginAuth2",
+        data=data,
+        headers=headers,
+        cookies=cookies
+    )
+
+    cookies.update(r.cookies.get_dict())
+    res=parse(r)
+
+    if res["code"]==70016:
+        exit("Invalid user or password")
+
+    region=json.loads(
+        requests.get(
+            "https://account.xiaomi.com/pass/user/login/region",
+            headers=headers,
+            cookies=cookies
+        ).text[11:]
+    )["data"]["region"]
+
+    nonce,ssecurity=res['nonce'],res['ssecurity']
+
+    res['location']+=f"&clientSign={quote(base64.b64encode(hashlib.sha1(f'nonce={nonce}&{ssecurity}'.encode()).digest()))}"
+
+    serviceToken=requests.get(
+        res['location'],
+        headers=headers,
+        cookies=cookies
+    ).cookies.get_dict()
+
+    micdata={
+        "userId":res['userId'],
+        "new_bbs_serviceToken":serviceToken["new_bbs_serviceToken"],
+        "region":region,
+        "deviceId":deviceId
     }
 
-    with open("micdata.json", "w") as f:
-        json.dump(micdata, f)
+    with open("micdata.json","w") as f:
+        json.dump(micdata,f)
 
     return micdata
 
+
 try:
     with open('micdata.json') as f:
-        micdata = json.load(f)
+        micdata=json.load(f)
 except:
-    micdata = login()
+    micdata=login()
 
-new_bbs_serviceToken = micdata["new_bbs_serviceToken"]
-deviceId = micdata["deviceId"]
-region = micdata['region']
+new_bbs_serviceToken=micdata["new_bbs_serviceToken"]
+deviceId=micdata["deviceId"]
+region=micdata["region"]
 
-print(f"\nAccount Region: {region}\n")
+print("Account Region:",region)
 
-api = "https://sgp-api.buy.mi.com/bbs/api/global/"
+api="https://sgp-api.buy.mi.com/bbs/api/global/"
 
-U_state = api + "user/bl-switch/state"
-U_apply = api + "apply/bl-auth"
-U_info = api + "user/data"
+U_state=api+"user/bl-switch/state"
+U_apply=api+"apply/bl-auth"
+U_info=api+"user/data"
 
-headers = {
-  'User-Agent': User,
-  'Content-Type': "application/json",
-  'Cookie': f"new_bbs_serviceToken={new_bbs_serviceToken};versionCode={versionCode};versionName={versionName};deviceId={deviceId};"
+headers={
+'User-Agent':User,
+'Content-Type':"application/json",
+'Cookie':f"new_bbs_serviceToken={new_bbs_serviceToken};versionCode={versionCode};versionName={versionName};deviceId={deviceId};"
 }
 
-info = requests.get(U_info, headers=headers).json()['data']
+info=requests.get(U_info,headers=headers).json()['data']
 
-print(f"Community Days: {info['registered_day']}")
-print(f"Level: {info['level_info']['level']}")
-print(f"Points: {info['level_info']['current_value']}")
+print("Community Days:",info['registered_day'])
+print("Level:",info['level_info']['level'])
+print("Points:",info['level_info']['current_value'])
 
 def state_request():
-    state = requests.get(U_state, headers=headers).json().get("data", {})
 
-    if state.get("is_pass") == 1:
-        exit("Unlock access already granted")
+    state=requests.get(U_state,headers=headers).json().get("data",{})
+
+    if state.get("is_pass")==1:
+        exit("Bootloader unlock permission already granted")
 
 state_request()
 
 def apply_request():
-    apply = requests.post(U_apply, data=json.dumps({"is_retry": True}), headers=headers)
 
-    if apply.json().get("code") != 0:
-        exit(apply.json())
+    apply=requests.post(
+        U_apply,
+        data=json.dumps({"is_retry":True}),
+        headers=headers
+    )
 
-    print("Application sent successfully.")
+    if apply.json().get("code")!=0:
+        print(apply.json())
+        return
 
-apply_request()
+    print("Unlock request sent successfully")
+
+def get_ntp_time():
+
+    client=ntplib.NTPClient()
+
+    try:
+        response=client.request("pool.ntp.org")
+        return datetime.fromtimestamp(response.tx_time,timezone.utc)
+    except:
+        return datetime.now(timezone.utc)
+
+def get_beijing_time():
+
+    utc_time=get_ntp_time()
+    return utc_time.astimezone(timezone(timedelta(hours=8)))
+
+def schedule_daily_task():
+
+    beijing_tz=timezone(timedelta(hours=8))
+
+    while True:
+
+        now=get_beijing_time()
+
+        target=now.replace(hour=23,minute=57,second=0,microsecond=0)
+
+        if now>=target:
+            target+=timedelta(days=1)
+
+        print("\nNext execution:",target)
+
+        while datetime.now(beijing_tz)<target:
+            time.sleep(60)
+
+        print("Sending unlock request...\n")
+
+        apply_request()
+
+while True:
+    schedule_daily_task()
